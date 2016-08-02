891c15fa15a23e08ee8135879077a2e28cacf24f
Manimaran Chandrasekaran <mani@dinventive.com>
2016-08-02 20:45:28 +0100

    Create loginazuresubscription.ps1

---
+++ tools/loginazuresubscription.ps1
@@ -0,0 +1,10 @@
+# Mandatory : update the username and the subscription id .
+# optional : change the password
+
+$username = 'username@directoryname.onmicrosoft.com'
+$subscriptionid = 'xxxxxxx-xxxxx-xxxx-xxxx-xxxxxx'
+$password =  'Ihatey0u!' | ConvertTo-SecureString -AsPlainText -Force
+$cred = new-object -typename System.Management.Automation.PSCredential `
+         -argumentlist $username, $password
+
+Add-AzureRmAccount -Credential $cred -SubscriptionId $subscriptionid
