04314cb8594a9e41e7f439b5a8423fdea9d516de
Ebru Cucen <ebrugul@hotmail.com>
2016-09-13 23:26:14 +0100

    Fixing ApplicationObjectID, spnRole, cleaning process
    ApplicationObjectID replace with ObjectId, missing spnRole parameter added as Contributor, missing param $appobjs for cleaning process added

--- tools/AzureADSPNConfig.ps1
+++ tools/AzureADSPNConfig.ps1
@@ -5,14 +5,12 @@ $homePage = 'http://devopspractice.com'
 $identifierUri = 'http://devopspractice.com'
 $password = 'hzkygExEcHe4xph38sdhOI37YB1GCzPoug0='
 
-
-
 $azureAdApplication = New-AzureRmADApplication -DisplayName $displayName -HomePage $homePage -IdentifierUris $identifierUri -Password $password -Verbose
 $appId = $azureAdApplication.ApplicationId
 Write-Output "Azure AAD Application creation completed successfully (Application Id: $appId)" -Verbose
 
 $spn = New-AzureRmADServicePrincipal -ApplicationId $appId
-$spnName = $spn.ServicePrincipalName
+$spnRole = "Contributor"
 
 Start-Sleep(20)
 Connect-MsolService -Credential $cred
@@ -21,7 +19,10 @@ New-MsolServicePrincipalCredential -AppPrincipalId $appId -type Password -StartD
 New-AzureRmRoleAssignment -RoleDefinitionName $spnRole -ServicePrincipalName $appId -Verbose
 
 #Get all the existing ad applications
-Get-AzureRmADApplication
+$appobjs= Get-AzureRmADApplication
 
-#Cleanup actions enable below if you wanted to clean up the azure ad application
-#$appobj | ForEach-Object { if ($_.ApplicationId.tostring().contains($appId)) { Remove-AzureRmADApplication -ApplicationObjectId $_.applicationobjectid -Force}}
+#Show the new Azure AD Application 
+$appobjs | ForEach-Object { if ($_.ApplicationId.tostring().contains($appId)) { $_}}
+
+##Cleanup actions enable below if you wanted to clean up the azure ad application
+#$appobjs | ForEach-Object { if ($_.ApplicationId.tostring().contains($appId)) { Remove-AzureRmADApplication -ObjectId $_.ObjectId -Force}}
