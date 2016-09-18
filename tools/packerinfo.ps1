099277e2b4c20867d45bb37ce7cf86e2087aa491
Ebru Cucen <ebrugul@hotmail.com>
2016-09-18 20:08:01 +0100

    spn file last part has the info
    carrying parameters into another file did not make sense...

--- tools/packerinfo.ps1
+++
@@ -1,9 +0,0 @@
-#This script will get all information required for the packer file
-
-$subscription = Get-AzureRmSubscription
-Write-Host 'Subscription Id :'  $subscription.SubscriptionId 
-Write-Host 'TenantId : '  $subscription.TenantId
-$appobj = Get-AzureRmADApplication -DisplayNameStartWith 'testdemo'
-Write-Host 'Objectid' $appobj[0].ObjectId.ToString()
-write-Host  'clientid' $appobj[0].ApplicationId.ToString()
-write-Host  'clientsecret Refer AzureADSPNConfig.ps1' 
